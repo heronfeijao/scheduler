6684c8ca2cb0d56ddca8de33c4436e9e02cb4577
Heron Feijao <heronfeijao@gmail.com>
2022-05-19 19:01:59 -0300

    finished day, days and appointments

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -5,73 +5,51 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList.jsx";
 import Appointment from "./Appointment/index.jsx";
-import Header from "./Appointment/Header.jsx";
-import Show from "./Appointment/Show.jsx";
-import Confirm from "./Appointment/Confirm.jsx";
-import Empty from "./Appointment/Empty.jsx";
-import Error from "./Appointment/Error.jsx";
-import Form from "./Appointment/Form.jsx";
-import Status from "./Appointment/Status.jsx";
-
-// --------------MOCK DATA---------------
-const appointments = {
-  1: {
-    id: 1,
-    time: "12pm",
-  },
-  2: {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      },
-    },
-  },
-  3: {
-    id: 3,
-    time: "2pm",
-  },
-  4: {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      },
-    },
-  },
-  5: {
-    id: 5,
-    time: "4pm",
-  },
-};
+import { getAppointmentsForDay } from "./helpers/selectors.js";
+// import Header from "./Appointment/Header.jsx";
+// import Show from "./Appointment/Show.jsx";
+// import Confirm from "./Appointment/Confirm.jsx";
+// import Empty from "./Appointment/Empty.jsx";
+// import Error from "./Appointment/Error.jsx";
+// import Form from "./Appointment/Form.jsx";
+// import Status from "./Appointment/Status.jsx";
 
 export default function Application(props) {
   // const [day, setDay] = useState([]);
   // const [days, setDays] = useState([]);
-  const appointmentList = Object.values(appointments).map((appointment) => {
-    return <Appointment key={appointment.id} {...appointment} />;
-  });
 
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
-  const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({...prev, days}))
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
+  // Object.values(appointments)
+
+  const setDay = (day) => setState({ ...state, day });
+  // const setDays = days => setState(prev => ({...prev, days}))
 
   useEffect(() => {
-    axios.get("/api/days").then((res) => setDays(res.data));
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers"),
+    ]).then((all) => {
+      setState((prev) => ({ ...prev, days: all[0].data, appointments: all[1].data }));
+      // setState((prev) => ({ ...prev, days: all[1].data }));
+      // setState((prev) => ({ ...prev, appointments: all[2].data }));
+      console.log('0', all[0].data); // first
+      console.log('1',all[1].data); // second
+      console.log('2',all[2].data); // third
+    });
   }, []);
+
+  const appointmentList = dailyAppointments.map((appointment) => {
+    return <Appointment key={appointment.id} {...appointment} />;
+  });
 
   return (
     <main className="layout">
