8d658cbd2cbaae89c6b7fe8ed3c9204fef3ee835
Heron Feijao <heronfeijao@gmail.com>
2022-05-19 17:50:07 -0300

    refactored the code for useState in application

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -54,11 +54,20 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [day, setDay] = useState([]);
-  const [days, setDays] = useState([]);
+  // const [day, setDay] = useState([]);
+  // const [days, setDays] = useState([]);
   const appointmentList = Object.values(appointments).map((appointment) => {
     return <Appointment key={appointment.id} {...appointment} />;
   });
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({...prev, days}))
 
   useEffect(() => {
     axios.get("/api/days").then((res) => setDays(res.data));
@@ -74,7 +83,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} value={day} onChange={setDay} />
+          <DayList days={state.days} value={state.day} onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
