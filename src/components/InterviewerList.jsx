af5aa9fea4baac8f85374c8e912f4be554cc2030
Heron Feijao <heronfeijao@gmail.com>
2022-05-24 14:55:45 -0300

    updated interviewerList to only receive interviewers as array

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem.jsx";
 import "./InterviewerList.scss";
+import PropTypes from "prop-types";
 
-export default function InterviewerList(props) {
+function InterviewerList(props) {
   const { value, onChange, interviewers } = props;
   const interviewerList = interviewers.map((interviewer) => {
     return (
@@ -23,3 +24,9 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired,
+};
+
+export default InterviewerList;
