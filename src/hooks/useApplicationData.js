f922b36496f6a11f4481e069875f0598295d3b77
Heron Feijao <heronfeijao@gmail.com>
2022-05-23 22:09:39 -0300

    code refactoring and organization

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
-import { updateSpots } from "../components/helpers/selectors";
+import { updateSpots } from "../components/helpers/updateSpots";
 
 export default function useApplicationData() {
   const [state, setState] = useState({
